29a90e0b8cb081f0caea57dbf0c748ae62c73e17
Saurabh Thakur <mail@saurabhthakur.dev>
2023-03-15 13:34:13 +0100

    Add structure to layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
-import '@/styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import type { AppProps } from 'next/app'
+import Layout from "@/components/Layout";
+import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
