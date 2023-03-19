3936fceb205c5acd0332d5ed1b8b70510a7b539a
Saurabh Thakur <mail@saurabhthakur.dev>
2023-03-20 00:24:45 +0100

    Add dynamic routing for multiple collections

---
+++ src/pages/[collection]/index.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+import PhotoGrid from "@/components/PhotoGrid";
+import CarouselModal from "@/components/CarouselModal";
+import config from "@/config";
+import Head from "next/head";
+
+export default function Spiti() {
+  const router = useRouter();
+  const [showCarousel, setShowCarousel] = useState(false);
+  const [clickedImage, setClickedImage] = useState(0);
+
+  const { collection } = router.query;
+  const collectionObject = config.collections.find(
+    (abc) => abc.slug === "/" + collection
+  ) ?? { pictures: [], title: "", description: "" };
+
+  function handleClickOnImage(index: number) {
+    setClickedImage(index);
+    setShowCarousel(true);
+  }
+
+  return (
+    <>
+      <Head>
+        <title>
+          {collectionObject.title} | {config.title}
+        </title>
+        <meta name="description" content={collectionObject.description} />
+      </Head>
+      <CarouselModal
+        show={showCarousel}
+        images={collectionObject.pictures}
+        openedImage={clickedImage}
+        handleClose={() => setShowCarousel(false)}
+      />
+      <PhotoGrid>
+        {collectionObject.pictures.map((image, index) => (
+          <img
+            key={image}
+            src={"/images/thumbs" + image}
+            alt={image}
+            onClick={() => handleClickOnImage(index)}
+          />
+        ))}
+      </PhotoGrid>
+    </>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import Head from "next/head";
-import Link from "next/link";
 
 import config from "@/config";
+import PhotoCard from "@/components/PhotoCard";
+import photogridStyles from "../components/PhotoGrid/PhotoGrid.module.css";
 
 export default function Home() {
   return (
@@ -12,8 +13,20 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>
-        <Link href="/spiti-valley">Spiti Valley</Link>
+
+      <div className={photogridStyles["photo-wrapper"]}>
+        <section id={photogridStyles.photos}>
+          {config.collections.map((collection) => (
+            <div key={collection.slug} style={{ padding: 8 }}>
+              <PhotoCard
+                title={collection.title}
+                slug={collection.slug}
+                description={collection.description}
+                cover={`/images/thumbs/${collection.slug}/${collection.cover}`}
+              />
+            </div>
+          ))}
+        </section>
       </div>
     </>
   );
