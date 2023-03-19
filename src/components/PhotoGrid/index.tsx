906f7673675c69b2d8547e860140b62a2b0105d8
Saurabh Thakur <mail@saurabhthakur.dev>
2023-03-20 00:23:55 +0100

    Update background colors

--- src/components/PhotoGrid/index.tsx
+++ src/components/PhotoGrid/index.tsx
@@ -1,32 +1,13 @@
-import Head from "next/head";
-
-import config from "@/config";
 import styles from "./PhotoGrid.module.css";
 
 interface PhotoGridProps {
-  name: string;
-  description: string;
   children: React.ReactNode;
 }
 
-export default function PhotoGrid({
-  name,
-  description,
-  children,
-}: PhotoGridProps) {
+export default function PhotoGrid({ children }: PhotoGridProps) {
   return (
-    <>
-      <Head>
-        <title>
-          {name} | {config.title}
-        </title>
-        <meta name="description" content={description} />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className={styles["photo-wrapper"]}>
-        <section id={styles.photos}>{children}</section>
-      </div>
-    </>
+    <div className={styles["photo-wrapper"]}>
+      <section id={styles.photos}>{children}</section>
+    </div>
   );
 }
