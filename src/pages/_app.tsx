435a7ec64c6c6330cb577f322b5f12673b92259e
Saurabh Thakur <mail@saurabhthakur.dev>
2023-03-19 20:59:53 +0100

    Remove bootstrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
+import type { AppProps } from "next/app";
+
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
