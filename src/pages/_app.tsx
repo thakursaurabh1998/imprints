3117fd95acab099c9dc67cbe5d3d9c7276f98045
Saurabh Thakur <mail@saurabhthakur.dev>
2023-04-15 22:18:09 +0200

    Add google tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,29 @@
+import Script from "next/script";
 import type { AppProps } from "next/app";
 
+import config from "@/config";
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${config.google_tag}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${config.google_tag}');
+        `}
+      </Script>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
